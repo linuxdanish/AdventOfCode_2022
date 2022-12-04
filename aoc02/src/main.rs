96fa8dcac945be539fd628d662a040a4c29bf534
Daniel Tate <whaaa14@gmail.com>
2022-12-04 12:46:31 -0500

    Day 2 part 2
    Correct answer 13022

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -6,7 +6,12 @@
 /// A, x = Rock = 1
 /// B, Y = Paper = 2
 /// C, Z = Scissors = 3
-/// 3 points for win
+/// 0 for loss, 3 for draw, 6 for win points for win
+///
+/// Part 2: x,y, and z now indicate lose, draw, win.
+/// Goal calculate score with new information.
+
+
 
 use std::env;
 use file_input;
@@ -35,7 +40,7 @@ fn main() {
     let content: Vec<String> = file_input::file_contents_as_vec(filename).expect("Failed to open input file");
 
     let option_map = | x:char | -> GameOption {
-        println!("char: {}",x );
+      //  println!("char: {}",x );
         match x.to_ascii_uppercase() {
             'A' | 'X' => {GameOption::Rock},
             'B' | 'Y' => {GameOption::Paper},
@@ -57,12 +62,68 @@ fn main() {
 
     // lets process our data:
     let score = rounds.iter().fold(0, | acc, x| -> i32 {
-        println!("DEBUG:: acc {}, result: {}, x.0: {}, x.1: {}", acc, challenge(x.0, x.1), x.0 as i32, x.1 as i32);
+       // println!("DEBUG:: acc {}, result: {}, x.0: {}, x.1: {}", acc, challenge(x.0, x.1), x.0 as i32, x.1 as i32);
         acc + (challenge(x.0, x.1) + ( x.1 as i32 + 1))
     });
 
     // result 
-    println!("The score if stategy is played is: {}", score);
+    println!("The score if (part 1) strategy is played is: {}", score);
+
+    // Part 2 processor
+
+
+    // function takes opponent's ' selection and chooses what we should play in response
+    let choose_play = | x: &str, y: &str | -> GameOption {
+
+        // New truth table with results in mind
+        //|          | Lose     | draw     | win      |
+        //| Rock     | Scissors | Rock     | Paper    |
+        //| Paper    | Rock     | Paper    | Scissors |
+        //| Scissors | Paper    | Scissors | Rock     |
+        let  tt2 = [
+            [GameOption::Scissors, GameOption::Rock, GameOption::Paper],
+            [GameOption::Rock, GameOption::Paper, GameOption::Scissors],
+            [GameOption::Paper, GameOption::Scissors, GameOption::Rock]
+        ];
+        // This is hokie, but we can re-use the same mapping as before for indexing
+        let new_index = | x: char | -> usize {
+             match x.to_ascii_uppercase() {
+            'A' | 'X' => {0},
+            'B' | 'Y' => {1},
+            'C' | 'Z' => {2},
+            _ => {panic!("invalid game option")}
+            }
+        };
+
+        let x = x.chars().next().unwrap();
+        let x = new_index(x);
+        let y = y.chars().next().unwrap();
+        let y = new_index(y);
+        // return the lookup
+        tt2[x][y]
+    };
+
+    let mut rounds2: Vec<(GameOption, GameOption)> = Vec::new();
+    for line in content.iter() {
+        let line = line.split_whitespace().collect::<Vec<&str>>();
+
+        if line.len() >= 2 {
+            let other_shape = option_map(line[0].chars().next().unwrap());
+            let our_shape = choose_play(line[0], line[1]);
+            rounds2.push((other_shape, our_shape));
+        }
+    };
+
+    // processing our data for the second round is the same as the first. Because we prepped the
+    // data to the same valid structure
+    // lets process our data:
+    let part2_score = rounds2.iter().fold(0, | acc, x| -> i32 {
+        // println!("DEBUG:: acc {}, result: {}, x.0: {}, x.1: {}", acc, challenge(x.0, x.1), x.0 as i32, x.1 as i32);
+        acc + (challenge(x.0, x.1) + ( x.1 as i32 + 1))
+    });
+
+    println!("The score if (part 2) strategy is played is: {}", part2_score);
+
 }
 
 // names for different options that can be selects
