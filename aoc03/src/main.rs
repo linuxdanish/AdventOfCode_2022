461f677f38091ef1b419ebfb3f8b7c47869a18ba
Daniel Tate <whaaa14@gmail.com>
2022-12-06 22:13:46 -0500

    Day 3 part 2
    Answer is 2604.
    
    I was in a rush, and this code is ugly.

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -2,7 +2,8 @@
 /// Daniel T. 2022-12-05
 /// Part 1 Goal: Locate the character repeated in the first half and second half
 ///              of each string. Then calculate score based on: a-z = [1-26] A-Z=[27-52]
-
+/// Part 2 Goal: For each set of three lines, locate the one character common to all three...
+///                then compute the score.
 
 use std::env;
 use file_input;
@@ -46,5 +47,42 @@ fn main() {
 
     println!("The final score is: {}", score);
 
+    // Part 2:
+    let mut counter = 0;
+    let mut badges: Vec<char> = Vec::new();
+    let mut candidates: HashMap<char, i32> = HashMap::new();
+    for line in content.iter() {
+    // sort and dedup
+        let mut sorted = line.chars().collect::<Vec<char>>();
+        sorted.sort_unstable();
+        sorted.dedup();
+        for y in sorted.iter() {
+            // if the key is already in the candidate pool, we increase the count.
+            // else we add it to the pool
+            if let Some(x) = candidates.get_mut(&y) {
+                *x = *x + 1;
+            } else {
+                candidates.insert(*y, 1);
+            }
+        }
+        // increase counter for group
+        counter = counter + 1;
+        if counter >= 3 {
+            // for every three lines, find the badge, then clear the candidate and reset the counter.
+            for (key, value) in candidates.iter() {
+                if value >= &3 {
+                    badges.push(*key);
+                    break;
+                }
+            }
+            candidates.clear();
+            counter = 0;
+        }
+    }
 
+    let score_part2 = badges.iter().fold(0, |acc, x| -> i32 {
+        acc + alphabet.chars().position(|y| x == &y).unwrap() as i32 + 1
+    });
+
+    println!("The part two score is: {}", score_part2);
 }
