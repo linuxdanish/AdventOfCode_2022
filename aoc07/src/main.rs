6bcc805f8f66b286a1ffe5cba383c928cd708f35
Daniel Tate <daniellawrencetate@gmail.com>
2022-12-30 14:07:25 -0500

    Day 7 part 1 works...
    answer 1517599
    This was dumb and annoying, but I decided to try an not use a tree for
    the file system. Mostly because I was avoiding the pain of working with
    general trees in Rust. This would be better had it used a tree (I
    believe)

--- aoc07/src/main.rs
+++ aoc07/src/main.rs
@@ -6,9 +6,8 @@
  *      What is the sum of the total sizes of those directories.
  */
 
- use std::{env::{self, Args}, str::SplitWhitespace};
+ use std::{env::{self}, str::SplitWhitespace};
  use file_input;
- use b_tree_lib;
  use std::collections::HashMap;
 
 
@@ -21,10 +20,9 @@ fn main() {
 
     // will build a hashmap of directories and their sizes
     // Maintain a stack of the current path 
-    let mut cur_path = Vec::<&str>::new();
-    let mut directories = HashMap::<&str, FsDirectory>::new();
-
-
+    let mut cur_path = Vec::<String>::new();
+    let mut directories = HashMap::<String, FsDirectory>::new();
+    let part1_max = 100000;
 
     for line in content.iter() {
         let mut line_parts = line.split_whitespace();
@@ -33,21 +31,61 @@ fn main() {
                 command_processor(line_parts, &mut cur_path, &mut directories);
             },
             Some("dir") => {
-
+                // build a new directory entry and add it to our list
+                // line should be of the form "dir <name>"
+                // I append the path of the cur_path to make each one unique
+                let dir_name = line_parts.next().unwrap();
+                let dir = FsDirectory{
+                    total_size: 0
+                };
+                let long_hash = cur_path.concat() + dir_name;
+                directories.insert(long_hash, dir);
             },
             Some(_) => {
-
+                // anything else should be a file line of the form:
+                // <size> <filename>
+                // we will add the file size to the current directory
+                if let Some(cur_dir_name) = cur_path.last() {
+                    if let Some(cur_dir) = directories.get_mut(&cur_dir_name.to_string()) {
+                        let mut line_part = line.split_whitespace();
+                        cur_dir.total_size += str::parse::<i32>(line_part.next().unwrap()).expect("failed to parse file size to i32");
+                    }
+                }
             },
             None => {}
         }
     }
+
+    // when done, I need to finish popping everything off of my cur_path stack:
+    while !cur_path.is_empty() {
+        println!("Debug: clean up remaining path items, cur_path len {}, directories count: {}", cur_path.len(), directories.len());
+        let old_dir_name = cur_path.pop().expect("failed to pop cur path");
+        let mut old_dir_size = 0;
+        if let Some(old_dir) = directories.get(&old_dir_name) {
+            old_dir_size = old_dir.total_size;
+        }
+        if let Some(new_dir_name) = cur_path.last(){
+            if let Some(new_dir) = directories.get_mut(new_dir_name) {
+                new_dir.total_size += old_dir_size;
+            }
+        }
+        
+    }
+
+    let candidates: Vec<(&String,&FsDirectory)> = directories.iter().filter(|x| x.1.total_size <= part1_max).collect();
+    let part1_result = candidates.iter()
+        .fold(0, |acc, x| {
+            acc + x.1.total_size
+        });
+
+    println!("Part1, sum of directories with a maximum size of 100,000: {}", part1_result);
 
 }
 
 // helper functions
 
 // Function takes a command line "cd, ls, etc." then handles the process of CDing into and out of a directory
-fn command_processor<'b>(mut line: SplitWhitespace<'b>, cur_path: &'b mut Vec<&'b str>, dirs: &'b mut HashMap<&'b str, FsDirectory<'b>> ) {
+fn command_processor<'b>(mut line: SplitWhitespace<'b>, cur_path: &mut Vec<String>, dirs: &mut HashMap<String, FsDirectory> ) {
     match line.next() {
         Some("cd") => {
             if let Some(arg) = line.next() {
@@ -55,26 +93,19 @@ fn command_processor<'b>(mut line: SplitWhitespace<'b>, cur_path: &'b mut Vec<&'
                     ".." => {
                         // Pop the current directory name off of the stack, lookup its size and add it to 
                         // the new directory's size
-                        let old_dir_size = dirs.get(cur_path.pop().unwrap()).unwrap().total_size;
-                        if let Some(new_dir_name) = cur_path.last(){
+                        print!("Debug: directory count {}", dirs.len());
+                        let old_dir_size = dirs.get(&cur_path.pop().unwrap()).unwrap().total_size;
+                        if let Some(new_dir_name) = cur_path.last() {
                             if let Some(new_dir) = dirs.get_mut(new_dir_name) {
-                                new_dir.total_size = old_dir_size;
+                                new_dir.total_size += old_dir_size;
                             }
-                        }                        
+                        }                      
+                        println!("Debug: directory count {}", dirs.len());
                     },
                     _ => {
                         // The argument is a directory name we will need to add it to our stack. 
-                        if let Some(dir) = dirs.get(arg) {
-                            cur_path.push(dir.name);
-                        } else {
-                            let mut dir = FsDirectory {
-                                name: arg.clone(),
-                                total_size: 0
-                            };
-                            dirs.insert(dir.name, dir);
-                            cur_path.push(dir.name);
-
-                        }
+                        let long_hash = cur_path.concat() + arg.clone();
+                        cur_path.push(long_hash);
                     }
                 }
             }
@@ -84,9 +115,8 @@ fn command_processor<'b>(mut line: SplitWhitespace<'b>, cur_path: &'b mut Vec<&'
 }
 
 
-#[derive(Copy, Clone)]
-struct FsDirectory<'a> {
-    name: &'a str,
+#[derive(Copy, Clone, Debug)]
+struct FsDirectory {
     total_size: i32,
 }
 
